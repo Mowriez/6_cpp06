aee1f74dc4ef01bed77c414a810a37cf0269cfaa
Maurice <mauricetrautner@gmail.com>
2023-12-04 20:15:31 +0100

    finished ex01

--- ex01/inc/Serializer.hpp
+++ ex01/inc/Serializer.hpp
@@ -13,12 +13,21 @@
 #ifndef SERIALIZER_HPP
 # define SERIALIZER_HPP
 
+# include <stdint.h>
+# include <iostream>
+# include <string>
+# include "Data.hpp"
+
 class Serializer {
 	private:
-
+		Serializer();
+		~Serializer();
+		Serializer(const Serializer &other);
+		Serializer& operator=(const Serializer &other);
 
 	public:
-	uint
+		static uintptr_t	serialize(Data* ptr);
+		static Data*		deserialize(uintptr_t raw);
 };
 
 #endif
