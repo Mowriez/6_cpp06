278a095e437c1d4b3d43be82bc45db8d4138903d
Maurice <mauricetrautner@gmail.com>
2023-12-04 18:15:13 +0100

    halfbaked ex01

--- ex01/inc/Serializer.hpp
+++ ex01/inc/Serializer.hpp
@@ -1,22 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Data.hpp                                           :+:      :+:    :+:   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtrautne <mtrautne@student.42wolfsburg.d>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/04 15:19:40 by mtrautne          #+#    #+#             */
-/*   Updated: 2023/12/04 15:19:41 by mtrautne         ###   ########.fr       */
+/*   Created: 2023/12/04 18:13:26 by mtrautne          #+#    #+#             */
+/*   Updated: 2023/12/04 18:13:56 by mtrautne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef DATA_HPP
-# define DATA_HPP
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
 
-# include <string>
+class Serializer {
+	private:
 
-struct Data {
-	std::string	exampleData;
+
+	public:
+	uint
 };
 
 #endif
